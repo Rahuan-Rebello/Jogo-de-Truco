f0a6e5ed5ea864b181cdcafb16e9e6b19853e5d8
Rahuan-Rebello <RahuanRebello@gmail.com>
2026-03-19 18:47:38 -0300

    Update of 'cartas.java' file

--- developed-features/Cartas.java
+++ developed-features/Cartas.java
@@ -1,16 +1,21 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Cartas {
 
-    private String[] cartas = {"4","5","6","7","Q","J","K","A","2","3"};
-    private String[] naipes = {"Ouros", "Espadas", "Copas", "Paus"};
+    private static final List<String> CARTAS = List.of("4","5","6","7","Q","J","K","A","2","3");
+    private static final List<String> NAIPES = List.of("Ouros", "Espadas", "Copas", "Paus");
 
-    public String[] getCartas()
+    public List<String> getCartas()
     {
-        return cartas;
+        return CARTAS;
     }
 
-    public String[] getNaipes()
+    public List<String> getNaipes()
     {
-        return naipes;
+        return NAIPES;
     }
 
 }   
