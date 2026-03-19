9275626b249fb5ed1fcc86b4402ec9854f51fd94
Rahuan-Rebello <RahuanRebello@gmail.com>
2026-03-18 21:25:15 -0300

    add Cartas file

---
+++ developed-features/Cartas.java
@@ -0,0 +1,16 @@
+public class Cartas {
+
+    private String[] cartas = {"4","5","6","7","Q","J","K","A","2","3"};
+    private String[] naipes = {"Ouros", "Espadas", "Copas", "Paus"};
+
+    public String[] getCartas()
+    {
+        return cartas;
+    }
+
+    public String[] getNaipes()
+    {
+        return naipes;
+    }
+
+}   
