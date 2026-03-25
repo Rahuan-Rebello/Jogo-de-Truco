5a9ea98dd1fa98e8f01103551e815124eebb5cd4
RAHUAN REBELLO ANVERSI DOS SANTOS <rahuan.rebello@cs.up.edu.br>
2026-03-25 20:08:58 -0300

    Update from 'Baralho.java', function 'Vira' and 'Manilha' has been added!

--- developed-features/Baralho.java
+++ developed-features/Baralho.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
-import java.util.List;
 import java.util.Collections;
+import java.util.List;
 
 public class Baralho 
 {
     private String valor;
     private String naipe;
     private int forca;
+    private Baralho vira;
     private Baralho[] baralho = new Baralho[40];
 
     public Baralho(String valor, String naipe, int forca)
@@ -46,5 +47,38 @@ public class Baralho
         System.out.println("As cartas foram embaralhadas");
     }
 
+public void definirVira() {
+    if (this.baralho != null && this.baralho.length > 0) {
+        this.vira = this.baralho[0]; 
+        System.out.println("--- VIRA DA RODADA ---");
+        System.out.println(vira.valor + " de " + vira.naipe);
+        System.out.println("A manilha é: " + identificarManilha());
+        System.out.println("-----------------------");
+    }
+}
+
+public String identificarManilha() {
+    List<String> ordem = Cartas.getCartas();
     
+    int indiceVira = ordem.indexOf(this.vira.valor);
+    
+    int indiceManilha = (indiceVira + 1) % ordem.size();
+    
+    return ordem.get(indiceManilha);
+}
+
+public int calcularForcaReal(String valorManilha) {
+
+    if (!this.valor.equals(valorManilha)) {
+        return this.forca; 
+    }
+    
+      switch (this.naipe) {
+        case "Paus":    return 100;
+        case "Copas":   return 90;
+        case "Espadas": return 80;
+        case "Ouros":   return 70;
+        default:        return this.forca;
+    }
+}    
 }
